dca44e399f4ae3cb2f66cb1ab8700efa3cb2fecf
Lukas Herman <lherman.cs@gmail.com>
2021-07-28 18:48:49 -0700

    Update node.sh

--- plugins/node.sh
+++ plugins/node.sh
@@ -8,6 +8,7 @@ function install_node() {
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 
   force_activate_nvm
+  nvm install 14
   nvm use 14
   npm install -g typescript typescript-language-server yarn
 }
