06bd12c711057acd4fe85e64529090798f055800
Lukas Herman <lherman.cs@gmail.com>
2021-12-28 08:50:54 -0500

    Use cmp

--- nvim/init.lua
+++ nvim/init.lua
@@ -9,6 +9,10 @@ function prequire(module, fn)
   end
 end
 
+function set(cmd)
+	vim.api.nvim_command('set ' .. cmd)
+end
+
 function setup_plugins()
 	-- Packer can manage itself
   use 'wbthomason/packer.nvim'
@@ -19,13 +23,18 @@ function setup_plugins()
 	}
 
 	use 'neovim/nvim-lspconfig'
-	use {
-    'hrsh7th/nvim-compe',
-    requires = {{'hrsh7th/vim-vsnip'}, {'hrsh7th/vim-vsnip-integ'}}
-  }
-	use 'folke/tokyonight.nvim'
-
-  use {
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-cmdline'
+  use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/cmp-vsnip'
+  use 'hrsh7th/vim-vsnip'
+  use 'onsails/lspkind-nvim'
+ 
+ 	use 'folke/tokyonight.nvim'
+ 
+   use {
     'hoob3rt/lualine.nvim',
     requires = {'kyazdani42/nvim-web-devicons'}
   }
@@ -51,85 +60,42 @@ function setup_lsps()
   require'lspconfig'.tsserver.setup{}
   require'lspconfig'.ccls.setup{}
 
-  vim.o.completeopt = 'menuone,noselect'
-
-  prequire('compe', function(m) m.setup {
-    enabled = true;
-    autocomplete = true;
-    debug = false;
-    min_length = 1;
-    preselect = 'enable';
-    throttle_time = 80;
-    source_timeout = 200;
-    resolve_timeout = 800;
-    incomplete_delay = 400;
-    max_abbr_width = 100;
-    max_kind_width = 100;
-    max_menu_width = 100;
-    documentation = {
-      border = { '', '' ,'', ' ', '', '', '', ' ' }, -- the border option is the same as `|help nvim_open_win|`
-      winhighlight = "NormalFloat:CompeDocumentation,FloatBorder:CompeDocumentationBorder",
-      max_width = 120,
-      min_width = 60,
-      max_height = math.floor(vim.o.lines * 0.3),
-      min_height = 1,
-    };
-
-    source = {
-      path = true;
-      buffer = true;
-      calc = true;
-      nvim_lsp = true;
-      nvim_lua = true;
-      vsnip = true;
-      ultisnips = true;
-      luasnip = true;
-    };
-  }
-  end)
-
-  local t = function(str)
-    return vim.api.nvim_replace_termcodes(str, true, true, true)
-  end
-
-  local check_back_space = function()
-      local col = vim.fn.col('.') - 1
-      return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
-  end
-
-  -- Use (s-)tab to:
-  --- move to prev/next item in completion menuone
-  --- jump to prev/next snippet's placeholder
-  _G.tab_complete = function()
-    if vim.fn.pumvisible() == 1 then
-      return t "<C-n>"
-    elseif vim.fn['vsnip#available'](1) == 1 then
-      return t "<Plug>(vsnip-expand-or-jump)"
-    elseif check_back_space() then
-      return t "<Tab>"
-    else
-      return vim.fn['compe#complete']()
-    end
-  end
-  _G.s_tab_complete = function()
-    if vim.fn.pumvisible() == 1 then
-      return t "<C-p>"
-    elseif vim.fn['vsnip#jumpable'](-1) == 1 then
-      return t "<Plug>(vsnip-jump-prev)"
-    else
-      -- If <S-Tab> is not working in your terminal, change it to <C-h>
-      return t "<S-Tab>"
-    end
-  end
-
-  vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
-  vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
-  vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
-  vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
-end
-
-function set(cmd)
-	vim.api.nvim_command('set ' .. cmd)
+  local cmp = require("cmp")
+  cmp.setup({
+     snippet = {
+        expand = function(args)
+           vim.fn["vsnip#anonymous"](args.body)
+        end,
+     },
+     mapping = {
+        ["<C-p>"] = cmp.mapping.select_prev_item(),
+        ["<C-n>"] = cmp.mapping.select_next_item(),
+        ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.close(),
+        ["<CR>"] = cmp.mapping.confirm({
+           behavior = cmp.ConfirmBehavior.Replace,
+           select = true,
+        }),
+        ["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "s" }),
+     },
+     formatting = {
+        format = function(_, vim_item)
+           vim.cmd("packadd lspkind-nvim")
+           vim_item.kind = require("lspkind").presets.codicons[vim_item.kind]
+           .. "  "
+           .. vim_item.kind
+           return vim_item
+        end,
+     },
+     sources = {
+        { name = "nvim_lsp" },
+        { name = "vsnip" },
+        { name = "path" },
+     },
+  })
 end
 
 function setup_editor() 
@@ -178,6 +144,9 @@ function setup_editor()
   set('cindent')         -- Like smartindent, but stricter and more customisable
 
   set('timeoutlen=1000 ttimeoutlen=0')
+
+  -- IDE related
+  set('completeopt=menu,menuone,noselect')
 end
 
 function setup_shortcuts()
