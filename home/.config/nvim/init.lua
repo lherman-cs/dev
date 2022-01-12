3d891eb5705052ec0e7ade9a4d9d2409685a9883
Lukas Herman <lherman.cs@gmail.com>
2022-01-12 14:48:41 -0500

    Add new cmp config

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -25,8 +25,10 @@ function setup_plugins()
   use 'hrsh7th/cmp-cmdline'
   use 'hrsh7th/nvim-cmp'
   use 'hrsh7th/cmp-vsnip'
-  use 'hrsh7th/vim-vsnip'
-  use 'onsails/lspkind-nvim'
+
+  -- For luasnip users.
+  use 'L3MON4D3/LuaSnip'
+  use 'saadparwaiz1/cmp_luasnip'
  
  	use 'folke/tokyonight.nvim'
 
@@ -49,47 +51,59 @@ function setup_plugins()
 end
 
 function setup_lsps()
-  require'lspconfig'.gopls.setup{}
-  require'lspconfig'.rust_analyzer.setup{}
-  require'lspconfig'.tsserver.setup{}
-  require'lspconfig'.ccls.setup{}
+  -- Setup nvim-cmp.
+  local cmp = require'cmp'
 
-  local cmp = require("cmp")
   cmp.setup({
-     snippet = {
-        expand = function(args)
-           vim.fn["vsnip#anonymous"](args.body)
-        end,
-     },
-     mapping = {
-        ["<C-p>"] = cmp.mapping.select_prev_item(),
-        ["<C-n>"] = cmp.mapping.select_next_item(),
-        ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(),
-        ["<C-e>"] = cmp.mapping.close(),
-        ["<CR>"] = cmp.mapping.confirm({
-           behavior = cmp.ConfirmBehavior.Replace,
-           select = true,
-        }),
-        ["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "s" }),
-     },
-     formatting = {
-        format = function(_, vim_item)
-           vim.cmd("packadd lspkind-nvim")
-           vim_item.kind = require("lspkind").presets.codicons[vim_item.kind]
-           .. "  "
-           .. vim_item.kind
-           return vim_item
-        end,
-     },
-     sources = {
-        { name = "nvim_lsp" },
-        { name = "vsnip" },
-        { name = "path" },
-     },
+    snippet = {
+      -- REQUIRED - you must specify a snippet engine
+      expand = function(args)
+        -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+        require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+        -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+      end,
+    },
+    mapping = {
+      ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+      ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+      ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+      ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+      ['<C-e>'] = cmp.mapping({
+        i = cmp.mapping.abort(),
+        c = cmp.mapping.close(),
+      }),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    },
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp' },
+      -- { name = 'vsnip' }, -- For vsnip users.
+      { name = 'luasnip' }, -- For luasnip users.
+      -- { name = 'ultisnips' }, -- For ultisnips users.
+      -- { name = 'snippy' }, -- For snippy users.
+    }, {
+      { name = 'buffer' },
+    })
   })
+
+  -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline('/', {
+    sources = {
+      { name = 'buffer' }
+    }
+  })
+
+  -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline(':', {
+    sources = cmp.config.sources({
+      { name = 'path' }
+    }, {
+      { name = 'cmdline' }
+    })
+  })
+
+  -- Setup lspconfig.
+  local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 end
 
 function setup_editor() 
@@ -190,14 +204,26 @@ prequire('lualine', function(m) m.setup{
 }
 end)
 prequire('gitsigns', function(m) m.setup() end)
-require('telescope').setup {
+prequire('telescope', function(m) m.setup{
+  defaults = {
+    vimgrep_arguments = {
+      'rg',
+      '--hidden',
+      '--color=never', 
+      '--no-heading',
+      '--with-filename',
+      '--line-number',
+      '--column', 
+      '--smart-case'
+    }
+  },
   pickers = {
     find_files = {
       hidden = true
     }
   }
 }
-
+end)
 setup_lsps()
 setup_editor()
 setup_shortcuts()
