d3c5968ff8d2f2d22d09ce8bf75b85daf95f9204
Lukas Herman <lherman.cs@gmail.com>
2022-04-17 10:03:46 -0400

    Add auto bootstrap

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -1,5 +1,11 @@
 local home_dir = os.getenv("HOME")
 
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
+
 function prequire(module, fn)
   local ok, res = pcall(require, module)
   if ok then
@@ -9,7 +15,13 @@ function prequire(module, fn)
   end
 end
 
-function setup_plugins()
+function setup_plugins(use)
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
--- install.sh
+++ install.sh
@@ -58,9 +58,6 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 # Install zsh awesome
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# Install NVIM package manager
-git clone https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-
 cargo install ripgrep
 
 link
