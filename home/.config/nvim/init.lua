866fe73413ad1f5d04960b71c35172eb365c64b9
Lukas Herman <lherman.cs@gmail.com>
2022-10-23 18:00:15 -0400

    Add toggleterm integration

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -67,6 +67,7 @@ function setup_plugins(use)
   use 'tpope/vim-sleuth'
   use { "rcarriga/nvim-dap-ui", requires = { "mfussenegger/nvim-dap" } }
   use 'simrat39/rust-tools.nvim'
+  use "akinsho/toggleterm.nvim"
 end
 
 function setup_lsps()
@@ -383,3 +384,31 @@ prequire('rust-tools', function(m)
     }
   })
 end)
+
+
+prequire("toggleterm", function(m)
+  m.setup {
+    -- size can be a number or function which is passed the current terminal
+    size = function(term)
+      if term.direction == "horizontal" then
+        return 15
+      elseif term.direction == "vertical" then
+        return vim.o.columns * 0.4
+      end
+    end,
+    open_mapping = [[<c-\>]],
+  }
+end)
+
+function _G.set_terminal_keymaps()
+  local opts = { buffer = 0 }
+  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+end
+
+-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
