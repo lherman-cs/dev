c0e797ec03604850e2d3f9a7206aa2ed1e8d7143
Lukas Herman <lherman.cs@gmail.com>
2022-10-23 20:06:47 -0400

    Use float instead of horizontal terminals

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -393,21 +393,22 @@ prequire("toggleterm", function(m)
       if term.direction == "horizontal" then
         return 15
       elseif term.direction == "vertical" then
-        return vim.o.columns * 0.4
+        return vim.o.columns * 0.25
       end
     end,
     open_mapping = [[<c-\>]],
+    direction = "float",
   }
 end)
 
 function _G.set_terminal_keymaps()
   local opts = { buffer = 0 }
   vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
-  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
-  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+  -- vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+  -- vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+  -- vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+  -- vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+  -- vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
 end
 
 -- if you only want these mappings for toggle term use term://*toggleterm#* instead
