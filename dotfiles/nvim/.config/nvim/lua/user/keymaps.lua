0fbddcd0cd89dde978cf1df6e87ad1ac8bf13ac5
Lukas Herman <lherman.cs@gmail.com>
2023-01-19 15:50:33 +0000

    This is a test commit

--- dotfiles/nvim/.config/nvim/lua/user/keymaps.lua
+++ dotfiles/nvim/.config/nvim/lua/user/keymaps.lua
@@ -64,5 +64,4 @@ map('n', '<leader>6', '6gt', options)
 map('n', '<leader>7', '7gt', options)
 map('n', '<leader>8', '8gt', options)
 map('n', '<leader>9', '9gt', options)
-map('n', '<leader>0', ':tablast<cr>', options)
 
