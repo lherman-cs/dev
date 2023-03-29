5a5606bbb77e8fb605cc58b5bf8cb9a90664a481
Lukas Herman <lukasman@amazon.com>
2023-03-28 21:21:48 -0400

    Refactor a bit

--- dotfiles/nvim/.config/nvim/lua/user/options.lua
+++ dotfiles/nvim/.config/nvim/lua/user/options.lua
@@ -71,9 +71,7 @@ function! g:EchoUrl(url)
     :echo a:url
 endfunction
 ]])
-function _G.mkdp_browserfunc(url)
-  print('hello ' .. url)
-end
 vim.g.mkdp_port = 8000
 vim.g.mkdp_open_to_the_world = 1
 vim.g.mkdp_browserfunc = 'g:EchoUrl'
+vim.g.mkdp_page_title = '${name}'
