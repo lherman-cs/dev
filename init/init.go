ce5b3dfd3a04b8f3f6cc3da625e7e78f6ab094a6
Lukas Herman <lherman.cs@gmail.com>
2023-04-01 13:09:33 -0400

    check for files before overwritting

--- init/init.go
+++ init/init.go
@@ -5,20 +5,24 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
+	"os"
 
 	"github.com/urfave/cli/v2"
 )
 
 //go:embed .envrc
 var envrcTemplate []byte
+
 const envrcFilename = ".envrc"
 
 //go:embed devshell.toml
 var devshellTemplate []byte
+
 const devshellFilename = "devshell.toml"
 
 //go:embed flake.nix
 var flakeTemplate []byte
+
 const flakeFilename = "flake.nix"
 
 func Command() *cli.Command {
@@ -30,6 +34,16 @@ func Command() *cli.Command {
 }
 
 func cmdInit(cliCtx *cli.Context) error {
+	if _, err := os.Stat(flakeFilename); err == nil {
+		return fmt.Errorf("file already exists: %s", flakeFilename)
+	}
+	if _, err := os.Stat(devshellFilename); err == nil {
+		return fmt.Errorf("file already exists: %s", devshellFilename)
+	}
+	if _, err := os.Stat(envrcFilename); err == nil {
+		return fmt.Errorf("file already exists: %s", envrcFilename)
+	}
+
 	err := errors.Join(
 		ioutil.WriteFile(flakeFilename, flakeTemplate, 0664),
 		ioutil.WriteFile(devshellFilename, devshellTemplate, 0664),
