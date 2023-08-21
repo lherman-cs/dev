e72541b48925f4c224453896e4418190fd8fd421
Lukas Herman <lherman.cs@gmail.com>
2023-08-21 15:15:36 -0400

    Use platform native packager

--- install.sh
+++ install.sh
@@ -3,30 +3,63 @@
 WORKSPACE_DIR=$HOME/workspace
 DEV_REPO_DIR=$WORKSPACE_DIR/dev
 
-# Install nix package manager
-sh <(curl -L https://nixos.org/nix/install) --yes
+## Install nix package manager
+#sh <(curl -L https://nixos.org/nix/install) --yes
+#
+## Load nix environment right away
+#if ! command -v nix-env &> /dev/null
+#then
+#	. $HOME/.nix-profile/etc/profile.d/nix.sh
+#fi
+#
+#
+## Install deps to nix
+#nix-channel --update
+#nix-env -iA \
+#	nixpkgs.git \
+#	nixpkgs.xclip \
+#	nixpkgs.htop \
+#	nixpkgs.neovim \
+#	nixpkgs.tmux \
+#	nixpkgs.stow \
+#	nixpkgs.curl \
+#	nixpkgs.fzf \
+#	nixpkgs.fd \
+#	nixpkgs.ripgrep \
+#	nixpkgs.go
+#
+function append_shell() {
+	if [ "$SHELL" = "bash" ]; then
+		echo "$1" >> ~/.bashrc
+	else
+		echo "$1" >> ~/.zshrc
+	fi
+}
 
-# Load nix environment right away
-if ! command -v nix-env &> /dev/null
-then
-	. $HOME/.nix-profile/etc/profile.d/nix.sh
+append_shell 'export PATH=$PATH:/opt/homebrew/bin'
+
+PACKAGES=(\
+	git \
+	htop \
+	neovim \
+	tmux \
+	stow \
+	curl \
+	fzf \
+	ripgrep \
+	go \
+)
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+	sudo apt update && sudo apt install -y ${PACKAGES[*]}
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	append_shell 'export PATH=$PATH:/opt/homebrew/bin'
+	/opt/homebrew/bin/brew install ${PACKAGES[*]}
+else
+        # Unknown.
+	echo "Unknown OSTYPE: $OSTYPE"
+	exit 1
 fi
-
-
-# Install deps to nix
-nix-channel --update
-nix-env -iA \
-	nixpkgs.git \
-	nixpkgs.xclip \
-	nixpkgs.htop \
-	nixpkgs.neovim \
-	nixpkgs.tmux \
-	nixpkgs.stow \
-	nixpkgs.curl \
-	nixpkgs.fzf \
-	nixpkgs.fd \
-	nixpkgs.ripgrep \
-	nixpkgs.go
 
 mkdir -p $WORKSPACE_DIR
 
@@ -44,11 +77,7 @@ git remote set-url origin git@github.com:lherman-cs/dev.git
 
 cd $DEV_REPO_DIR/dotfiles
 stow --target=$HOME --verbose --restow --no-folding */
+append_shell "source '$HOME/.extend.rc'"
 
-if [ "$SHELL" = "bash" ]; then
-	echo "source '$HOME/.extend.rc'" >> ~/.bashrc
-else
-	echo "source '$HOME/.extend.rc'" >> ~/.zshrc
-fi
-
-curl -sf https://gobinaries.com/lherman-cs/dev | sh
+# curl -sf https://gobinaries.com/lherman-cs/dev | sh
+go install .
