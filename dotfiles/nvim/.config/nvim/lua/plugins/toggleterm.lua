aec49ba64a17bec786a981f6402b55f42b09643b
Lukas Herman <lherman.cs@gmail.com>
2023-09-20 15:49:17 -0400

    fix: toggleterm doesn't resume session

--- dotfiles/nvim/.config/nvim/lua/plugins/toggleterm.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/toggleterm.lua
@@ -1,15 +1,18 @@
 return {
-    {
+  {
     "akinsho/toggleterm.nvim",
-    config = function()
-      require("toggleterm").setup({
-        open_mapping = [[<c-\>]],
-        direction = "float"
-      })
-    end,
-    keys = {
-      { [[<c-\>]] },
+    config = true,
+    cmd = "ToggleTerm",
+    keys = { { [[<c-\>]], "<cmd>ToggleTerm<cr>", desc = "Toggle floating terminal" } },
+    opts = {
+      open_mapping = [[<c-\>]],
+      direction = "float",
+      shade_filetypes = {},
+      hide_numbers = true,
+      insert_mappings = true,
+      terminal_mappings = true,
+      start_in_insert = true,
+      close_on_exit = true,
     },
   },
 }
-
