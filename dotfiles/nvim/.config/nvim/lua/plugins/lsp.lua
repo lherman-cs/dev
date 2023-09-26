5a6082da8ec7f5eacdba4e07bd25af1ab36e444f
Lukas Herman <lherman.cs@gmail.com>
2023-09-26 13:05:50 -0400

    fix: tweak kotlin config for bigger projects

--- dotfiles/nvim/.config/nvim/lua/plugins/lsp.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/lsp.lua
@@ -3,6 +3,13 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       autoformat = false,
+      servers = {
+        kotlin_language_server = {
+          settings = {
+            ['kotlin.java.opts'] = '-Xmx8g'
+          }
+        }
+      }
     },
   },
 }
