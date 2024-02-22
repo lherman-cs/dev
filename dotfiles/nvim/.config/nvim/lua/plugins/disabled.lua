a60cb3b8298bc46e78fa24b457ce171aacdfa229
Lukas Herman <lherman.cs@gmail.com>
2024-02-21 22:26:28 -0500

    Disable auto pair quotes

--- dotfiles/nvim/.config/nvim/lua/plugins/disabled.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/disabled.lua
@@ -2,4 +2,5 @@ return {
   --{ "akinsho/bufferline.nvim", enabled = false },
   { "nvim-treesitter/nvim-treesitter", enabled = false },
   { "windwp/nvim-ts-autotag", enabled = false },
+  { "echasnovski/mini.pairs", enabled = false },
 }
