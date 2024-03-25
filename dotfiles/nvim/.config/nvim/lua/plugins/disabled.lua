2c9ded7ef6594b8676d5a786988b7f4b09a58d14
Lukas Herman <lherman.cs@gmail.com>
2024-03-25 10:30:59 -0400

    reenable treesitter

--- dotfiles/nvim/.config/nvim/lua/plugins/disabled.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/disabled.lua
@@ -1,6 +1,6 @@
 return {
   --{ "akinsho/bufferline.nvim", enabled = false },
-  { "nvim-treesitter/nvim-treesitter", enabled = false },
+  -- { "nvim-treesitter/nvim-treesitter", enabled = false },
   { "windwp/nvim-ts-autotag", enabled = false },
   { "echasnovski/mini.pairs", enabled = false },
 }
