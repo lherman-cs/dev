b6c284461c696e2c05e33ed30e762e585dfc2531
Lukas Herman <lherman.cs@gmail.com>
2024-06-09 10:12:21 -0400

    use os.Remove

--- dev.go
+++ dev.go
@@ -383,7 +383,7 @@ func cmdLinkHandler(from, to string, real, force bool) error {
 					return fmt.Errorf("%s already exists. use -force to override", newPath)
 				}
 
-				err = os.RemoveAll(newPath)
+				err = os.Remove(newPath)
 				if err != nil {
 					return fmt.Errorf("failed to delete %s: %w", newPath, err)
 				}
