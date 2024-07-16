486874412c533202d159ed98aee1dd0de9575a68
Lukas Herman <lherman.cs@gmail.com>
2024-07-16 14:38:29 -0400

    cut off plugins

--- dotfiles/.config/nvim/lua/plugins/core.lua
+++ dotfiles/.config/nvim/lua/plugins/core.lua
@@ -1,32 +1,9 @@
 return {
+  { "windwp/nvim-ts-autotag", enabled = false },
+  { "echasnovski/mini.pairs", enabled = false },
   {
     "tpope/vim-fugitive",
     cmd = "G",
-  },
-  { "tpope/vim-surround" },
-  {
-    "akinsho/toggleterm.nvim",
-    config = true,
-    cmd = "ToggleTerm",
-    keys = { { [[<c-\>]], "<cmd>ToggleTerm<cr>", desc = "Toggle floating terminal" } },
-    opts = {
-      open_mapping = [[<c-\>]],
-      direction = "float",
-      shade_filetypes = {},
-      hide_numbers = true,
-      insert_mappings = true,
-      terminal_mappings = true,
-      start_in_insert = true,
-      close_on_exit = true,
-    },
-  },
-  {
-    "rcarriga/nvim-notify",
-    opts = {
-      max_width = function()
-        return math.floor(vim.o.columns * 0.75)
-      end,
-    },
   },
   { "catppuccin/nvim", opts = { flavour = "latte" } },
   {
--- dotfiles/.config/nvim/lua/plugins/disabled.lua
+++
@@ -1,6 +0,0 @@
-return {
-  --{ "akinsho/bufferline.nvim", enabled = false },
-  -- { "nvim-treesitter/nvim-treesitter", enabled = false },
-  { "windwp/nvim-ts-autotag", enabled = false },
-  { "echasnovski/mini.pairs", enabled = false },
-}
