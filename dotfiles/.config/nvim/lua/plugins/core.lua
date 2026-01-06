b116d2d847fa04e5f893e2ada21de4bd1f0d26c6
Lukas Herman <lherman.cs@gmail.com>
2026-01-06 07:10:45 -0500

    remove transparent theme

--- dotfiles/.config/nvim/lua/plugins/core.lua
+++ dotfiles/.config/nvim/lua/plugins/core.lua
@@ -9,21 +9,21 @@ return {
   -- {
   --   "rebelot/kanagawa.nvim",
   -- },
-  {
-    "folke/tokyonight.nvim",
-    opts = {
-      transparent = true,
-      styles = {
-        sidebars = "transparent",
-        floats = "transparent",
-      },
-    },
-  },
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   opts = {
+  --     transparent = true,
+  --     styles = {
+  --       sidebars = "transparent",
+  --       floats = "transparent",
+  --     },
+  --   },
+  -- },
   {
     "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "tokyonight",
-    },
+    -- opts = {
+    --   colorscheme = "tokyonight",
+    -- },
   },
   {
     "nvim-telescope/telescope.nvim",
